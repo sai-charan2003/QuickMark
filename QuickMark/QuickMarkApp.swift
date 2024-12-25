5ebb500bab636023d07d65d78ca70557dadea4df
Sai Charan <saicharannallapaneni@gmail.com>
2024-12-25 11:52:06 +0530

    Initial Commit

---
+++ QuickMark/QuickMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMarkApp.swift
+//  QuickMark
+//
+//  Created by Sai Charan on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct QuickMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
